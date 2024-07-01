ab6b7077ee994f4f1c014f99fb2b299a07229b59
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-07-02 01:57:38 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const url ="https://api.wazirx.com/api/v2/tickers";
+const getData = async()=>{
+    console.log("getting data...");
+    let response = await fetch(url);
+    console.log(response);
+}
